b97b4bdd4b9d223adc6c53787b5bc7663af07c50
Ahmed Raza <artistahmed15@gmail.com>
2021-01-01 16:09:13 +0530

    Commit after error boundry

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,21 +3,18 @@ import Card from './Card';
 
 
 const CardList = ({ robots }) => {
-   return (
-      <div >
-         {
+    return ( <div> {
             robots.map((user, i) => {
-               return (
-                  <Card
-                     key={i}
-                     id={robots[i].id}
-                     name={robots[i].name}
-                     email={robots[i].email}
-                  />
-               );
-            })}
-      </div>
-   );
+                return ( <
+                    Card key = { i }
+                    id = { robots[i].id }
+                    name = { robots[i].name }
+                    email = { robots[i].email }
+                    />
+                );
+            })
+        } </div>
+    );
 }
 
 export default CardList;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import './App.css'
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css';
 
 class App extends Component {
     constructor() {
@@ -25,25 +26,25 @@ class App extends Component {
     }
 
     render() {
-        const  { robots, searchfield } = this.state;
+        const { robots, searchfield } = this.state;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        return !robots.length ? 
-        <h1>Loading</h1> : 
-        ( 
-               <div className = 'tc' >
-                <h1 className = 'f1' > RoboFriends </h1> 
-                <SearchBox searchChange = { this.onSearchChange }
-                /> 
-                <Scroll >
+        return !robots.length ?
+            <h1> Loading </h1> : 
+            ( <div className = 'tc' >
+                <h1 className = 'f1' > RoboFriends </h1>  <
+                SearchBox searchChange = { this.onSearchChange }
+                />  <Scroll>
+                <ErrorBoundry>
                 <
                 CardList robots = { filteredRobots }
-                /> 
-                </Scroll> 
+                />  
+                </ErrorBoundry>
+                </Scroll>  
                 </div>
             );
-        }
+    }
 }
 
 
